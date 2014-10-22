6dedda7dee77366d0519b8817680b1a02bb2b7a3
clairtonluz <clairton.c.l@gmail.com>
2014-10-22 19:50:57 -0300

    chat enviando mensagem

---
+++ src/chat/ChatController.java
@@ -0,0 +1,27 @@
+package chat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ChatController {
+
+    @FXML
+    private Button bt_enviar;
+
+    @FXML
+    private TextArea ta_conversa;
+
+    @FXML
+    private TextField tf_mensagem;
+
+    @FXML
+    void enviar(ActionEvent event) {
+        System.out.println(tf_mensagem.getText());
+        ta_conversa.setText(tf_mensagem.getText());
+
+    }
+
+}
--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package chat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
