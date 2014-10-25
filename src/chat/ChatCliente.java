5b299e3070b54ce240b830cb65e09488d3fa757f
clairtonluz <clairton.c.l@gmail.com>
2014-10-24 21:29:14 -0300

    enviando a mensgem com data e hora

--- src/chat/ChatCliente.java
+++ src/chat/ChatCliente.java
@@ -4,60 +4,42 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ChatCliente {
 
+    private String username = "Clairton";
     private int port;
-    private String ipServer;
+    private String hostname;
+    private Socket clientSocket;
+    private DateTimeFormatter formatter;
 
     public ChatCliente() {
-        this.ipServer = "localhost";
-        this.port = 1024;
+        this("localhost", 9000);
     }
 
-    public ChatCliente(String ipServer, int port) {
-        this.ipServer = ipServer;
+    public ChatCliente(String hostname, int port) {
+        this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        this.hostname = hostname;
         this.port = port;
     }
 
-
     public void enviar(String mensagem) {
+        try {
+            this.clientSocket = new Socket(hostname, port);
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+            PrintStream out = new PrintStream(clientSocket.getOutputStream());
+            DataInputStream userInput = new DataInputStream(System.in);
 
-        while (true) {
-            try (Socket cliente = new Socket(hostname, port)) {
-                DataInputStream in = new DataInputStream(cliente.getInputStream());
-                PrintStream out = new PrintStream(cliente.getOutputStream());
-                DataInputStream userInput = new DataInputStream(System.in);
-
-                if ((mensagem == null) || mensagem.isEmpty()) {
-                    break;
-                }
-                out.println(mensagem);
-
-                while ((mensagem = in.readLine()) != null) {
-                    System.out.println(mensagem);
-                }
-            } catch (IOException e) {
-                System.err.println(e);
+            if ((mensagem != null) || !mensagem.isEmpty()) {
+                out.println(String.format("%s [%s]: %s",this.username, LocalDateTime.now().format(formatter), mensagem));
             }
+
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
-//    public String enviar(String mensagem) {
-//        System.out.println("ENVIAR " + mensagem);
-//        try (Socket cliente = new Socket(ipServer, port)) {
-//            PrintStream out = new PrintStream(cliente.getOutputStream());
-//            if (mensagem == null || mensagem.isEmpty()) {
-//                out.println(mensagem);
-//                return mensagem;
-//            }
-//        } catch (UnknownHostException e) {
-//            return "Host desconhecido";
-//        } catch (IOException e) {
-//            return "Host desconhecido";
-//        }
-//        return null;
-//    }
 
     public int getPort() {
         return port;
@@ -67,11 +49,11 @@ public class ChatCliente {
         this.port = port;
     }
 
-    public String getIpServer() {
-        return ipServer;
+    public String getHostname() {
+        return hostname;
     }
 
-    public void setIpServer(String ipServer) {
-        this.ipServer = ipServer;
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
     }
 }
