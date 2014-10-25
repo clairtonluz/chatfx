2eb1a9350032e6871f329fcb03551a3c22826e2a
clairtonluz <clairton.c.l@gmail.com>
2014-10-25 12:01:48 -0300

    refatorando

--- src/chat/ChatController.java
+++ src/chat/ChatController.java
@@ -21,40 +21,13 @@ public class ChatController {
     private Thread TaskMonitorarMensage;
 
     public void initialize() {
-        System.out.println("INIT");
         cliente = new ChatCliente(ta_conversa);
-        aguardarMensagens(ta_conversa);
     }
 
     @FXML
     void enviar(ActionEvent event) {
         cliente.enviar(tf_mensagem.getText());
         tf_mensagem.clear();
-
-    }
-
-    private void aguardarMensagens(final TextArea receptorDaMensagem) {
-        TaskMonitorarMensage = new Thread(() -> {
-            try {
-                // Ler mensagem do servidor e imprime
-                String mensagem;
-                final StringBuilder sb = new StringBuilder();
-                while ((mensagem = cliente.getInput().readLine()) != null) {
-                    System.out.println(mensagem);
-                    sb.append(mensagem).append(System.getProperty("line.separator"));
-                    Platform.runLater(() -> {
-                        receptorDaMensagem.setText(sb.toString());
-                    });
-                }
-            } catch (IOException e) {
-                System.err.println("Connection to server broken.");
-                e.printStackTrace();
-            }
-        });
-        TaskMonitorarMensage.start();
-    }
-
-    public void sair(){
 
     }
 
--- src/chat/cliente/ChatCliente.java
+++ src/chat/cliente/ChatCliente.java
@@ -1,5 +1,6 @@
 package chat.cliente;
 
+import javafx.application.Platform;
 import javafx.scene.control.TextArea;
 
 import java.io.*;
@@ -13,15 +14,17 @@ public class ChatCliente {
     public static final String SERVER_HOSTNAME = "localhost";
     public static final int SERVER_PORT = 8080;
 
-    public BufferedReader getInput() {
-        return input;
-    }
-
     private BufferedReader input = null;
     private PrintWriter output = null;
     public static final StringBuilder CONVERSA = new StringBuilder();
 
-    public ChatCliente(TextArea textArea) {
+    public ChatCliente(TextArea receptorDaMensagem) {
+        conectar();
+
+        aguardarMensagens(receptorDaMensagem);
+    }
+
+    private void conectar() {
         try {
             Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -34,35 +37,25 @@ public class ChatCliente {
         }
     }
 
-//    public static void main(String[] args) {
-//        BufferedReader in = null;
-//        PrintWriter out = null;
-//        try {
-//            Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
-//            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-//            System.out.printf("Conectado ao servidor %s:%s", SERVER_HOSTNAME, SERVER_PORT);
-//        } catch (IOException e) {
-//            System.err.println("Can not establish connection to " + SERVER_HOSTNAME + ":" + SERVER_PORT);
-//            e.printStackTrace();
-//            System.exit(-1);
-//        }
-//
-//        Enviador enviador = new Enviador(out);
-//        enviador.setDaemon(true);
-//        enviador.start();
-//
-//        try {
-//            // Ler mensagem do servidor e imprime
-//            String message;
-//            while ((message = in.readLine()) != null) {
-//                System.out.println(message);
-//            }
-//        } catch (IOException e) {
-//            System.err.println("Connection to server broken.");
-//            e.printStackTrace();
-//        }
-//    }
+    private void aguardarMensagens(TextArea receptorDaMensagem){
+        new Thread(() -> {
+            try {
+                // Ler mensagem do servidor e imprime
+                String mensagem;
+                final StringBuilder sb = new StringBuilder();
+                while ((mensagem = input.readLine()) != null) {
+                    System.out.println(mensagem);
+                    sb.append(mensagem).append(System.getProperty("line.separator"));
+                    Platform.runLater(() -> {
+                        receptorDaMensagem.setText(sb.toString());
+                    });
+                }
+            } catch (IOException e) {
+                System.err.println("Connection to server broken.");
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     public void enviar(String mensagem) {
         output.println(mensagem);
